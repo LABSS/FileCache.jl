a2883249b508533d43556764c078eeeff851cc09
Nicolas Payette <nicolaspayette@gmail.com>
2018-01-05 18:08:26 +0100

    add FileCache code

--- src/FileCache.jl
+++ src/FileCache.jl
@@ -1,5 +1,20 @@
 module FileCache
 
-# package code goes here
+  export maybeload
+
+  type CachedFile
+    mtime::Float64
+    content::Any
+  end
+
+  const cache = Dict{String, Any}()
+
+  function maybeload(f::Function, path::AbstractString)
+    p = realpath(path)
+    if !(haskey(cache, p) && mtime(p) == cache[p].mtime)
+      cache[p] = CachedFile(mtime(p), f(p))
+    end
+    cache[p].content
+  end
 
 end # module
